b5e7ebe82e7db763a800475df98b0f0bfaafd750
snowflaketrainingashishdixit <117939878+snowflaketrainingashishdixit@users.noreply.github.com>
2022-11-10 18:50:56 +0000

    Update streamlit_app.py
    imported panda and added fruitl list

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text ('🐔Veg Sausages')
 streamlit.text (' 🥑Scrambled Eggs')
 streamlit.text ('🍞 Mushrooms')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
