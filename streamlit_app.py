5d66a6d5cb1805f7de82c0dbf2f0a0cafbcb490f
snowflaketrainingashishdixit <117939878+snowflaketrainingashishdixit@users.noreply.github.com>
2022-11-10 18:47:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-streamlit.title('My Parents New healthy diner')
+streamlit.title('My Parents New healthy diner 
+
+')
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text ('🥗 Hash Browns')
 streamlit.text ('🐔Veg Sausages')
