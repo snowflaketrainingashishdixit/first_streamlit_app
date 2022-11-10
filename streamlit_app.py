e27140c552a803310d40fcf1b2f530f3ba602ad2
snowflaketrainingashishdixit <117939878+snowflaketrainingashishdixit@users.noreply.github.com>
2022-11-10 18:44:45 +0000

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text ('Hash Browns')
+streamlit.text ('Veg Sausages')
+streamlit.text ('Scrambled Eggs')
+streamlit.text ('Mushrooms')
