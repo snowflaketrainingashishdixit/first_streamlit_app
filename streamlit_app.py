7999d16076d20c689136abf5521261b2b37b133f
snowflaketrainingashishdixit <117939878+snowflaketrainingashishdixit@users.noreply.github.com>
2022-11-14 22:33:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -28,8 +28,9 @@ try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information")
-  else:
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
