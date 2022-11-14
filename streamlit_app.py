3f8ad6b8e535b69d6473547ad765cf322dde282c
snowflaketrainingashishdixit <117939878+snowflaketrainingashishdixit@users.noreply.github.com>
2022-11-14 22:16:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruityvice")
+my_cur.execute("select "1" from fruityvice")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
@@ -46,9 +46,9 @@ streamlit.dataframe(my_data_row)
 
 
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), ['Avocado', 'Strawberries'])
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), ['Avocado', 'Strawberries'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(fruits_to_show)
-streamlit.dataframe(fruits_selected)
+#streamlit.dataframe(fruits_selected)
 
 
