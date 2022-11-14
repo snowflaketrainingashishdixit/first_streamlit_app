c14dafb78d8b4f59da97af10f904725eb3bfcebd
snowflaketrainingashishdixit <117939878+snowflaketrainingashishdixit@users.noreply.github.com>
2022-11-14 09:32:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,13 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # Display the table on the page
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), ['Avocado', 'Strawberries'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(fruits_selected)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
